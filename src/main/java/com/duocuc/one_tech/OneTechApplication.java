b73e737b15b07d09290f3947a1490f2735078aea
solgrey <sol.medina@duocuc.cl>
2025-11-11 12:07:42 -0300

    First Commit

---
+++ src/main/java/com/duocuc/one_tech/OneTechApplication.java
@@ -0,0 +1,13 @@
+package com.duocuc.one_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duocuc/one_tech/OneTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duocuc.one_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
