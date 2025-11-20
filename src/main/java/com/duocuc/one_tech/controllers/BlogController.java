bd11ae06d250f005c5cc9853f9edf73cecb0f213
Solgrey <sol.medina@duocuc.cl>
2025-11-19 23:16:28 -0300

    Se agregan buenas practicas a APIS

--- src/main/java/com/duocuc/one_tech/controllers/BlogController.java
+++ src/main/java/com/duocuc/one_tech/controllers/BlogController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/blog")
+@RequestMapping("/api/v1/blog")
 @CrossOrigin(origins = "*")
 public class BlogController {
 
